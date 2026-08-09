98f0c48f8ecdd24b18280f78e4215255fcfc49aa
Ryan Mann <ryan.randall.mann@gmail.com>
2026-08-08 18:18:48 -0600

    LeetCode 1140
    Initial commit for Stone Game II.. more Alice and bob with their rock shenanigans

---
+++ medium/stoneGameII.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int stoneGameII(int[] piles) {
+        // another alice and bob mudfest
+        // we know they play optimally.. again
+        // alice starts first..
+        // alice initially has the chance to chose 1 element or 2 elements(M = 1)
+        // as she chooses 2 elements.. M = 2.. if bob chooses 3.. M = 3? and so on?
+        // so each player can choose to *limit* the next player by choosing a lower amount of stone piles vs maximizing their own..
+        // alice has first decision making power but not necessarily an absolute value to win...
+    }
+}
